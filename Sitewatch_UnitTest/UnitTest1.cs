ef997f69c34ced6e7b644fbcd79641829b9e3007
111192973+maganadev@users.noreply.github.com <111192973+maganadev@users.noreply.github.com>
2023-08-23 18:15:07 -0700

    Update: Adding unit test to confirm behavior

--- Sitewatch_UnitTest/UnitTest1.cs
+++ Sitewatch_UnitTest/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Sitewatch;
 using Sitewatch.OOP;
 using Sitewatch.JSON;
+using System.Diagnostics.Metrics;
 
 namespace Sitewatch_UnitTest
 {
@@ -11,12 +12,6 @@ namespace Sitewatch_UnitTest
         {
             Program.applyLogConfig();
             PuppeteerSingleton.init().GetAwaiter().GetResult();
-
-            SitewatchTaskConfig taskConfig = new SitewatchTaskConfig();
-            SitewatchTask task = new SitewatchTask(taskConfig, "dummy");
-
-            Program.tasks.Add(task);
-            Program.launchTasks();
         }
 
         [Test]
@@ -39,6 +34,110 @@ namespace Sitewatch_UnitTest
             //Check TruncateString
             var result = Safety.TruncateString("text.txt", "dfkdslfjkdls;fds");
             Assert.True(result == "text.txt");
+        }
+
+        [Test]
+        public async Task ForgettingHistory()
+        {
+            const string TaskName = "ForgettingHistory";
+
+            int count = 0;
+            string[] history = {
+                @"<body>
+                    <food type=""apple""></food>
+                    <food type=""banana""></food>
+                    <food type=""carrots""></food>
+                </body>",
+                @"<body>
+                    <food type=""apple""></food>
+                    <food type=""banana""></food>
+                </body>",
+                @"<body>
+                    <food type=""apple""></food>
+                    <food type=""banana""></food>
+                    <food type=""carrots""></food>
+                </body>",
+            };
+            Dictionary<string,bool> emptyMap = new Dictionary<string,bool>();
+
+            SitewatchTaskConfig taskConfig = new SitewatchTaskConfig();
+            taskConfig.URL = "";
+            taskConfig.QuerySelectorAll_Query = "food";
+            taskConfig.PreprocessSteps = new List<PreprocessStep>();
+            taskConfig.ShouldWatchForAdditions = true;
+            taskConfig.ShouldWatchForDeletions = true;
+            taskConfig.ShouldWatchForNoChanges = true;
+            taskConfig.UpdateCheckIntervalSeconds = 1;
+            taskConfig.ShouldForgetDeletions = true;
+            taskConfig.name = TaskName;
+
+            //Clear any previous tests
+            await Safety.setArchivedSiteContent(taskConfig, emptyMap, emptyMap, emptyMap);
+
+            //Set initial
+            await Program.HandleUpdateLogic(taskConfig, history[0]);
+
+            //Should now only contain 2
+            await Program.HandleUpdateLogic(taskConfig, history[1]);
+            count = (await Safety.getArchivedSiteContent(TaskName)).Count;
+            Assert.That(count, Is.EqualTo(2));
+
+            //Should now contain 3
+            await Program.HandleUpdateLogic(taskConfig, history[2]);
+            count = (await Safety.getArchivedSiteContent(TaskName)).Count;
+            Assert.That(count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public async Task NotForgettingHistory()
+        {
+            const string TaskName = "NotForgettingHistory";
+
+            int count = 0;
+            string[] history = {
+                @"<body>
+                    <food type=""apple""></food>
+                    <food type=""banana""></food>
+                    <food type=""carrots""></food>
+                </body>",
+                @"<body>
+                    <food type=""apple""></food>
+                    <food type=""banana""></food>
+                </body>",
+                @"<body>
+                    <food type=""apple""></food>
+                    <food type=""banana""></food>
+                    <food type=""carrots""></food>
+                </body>",
+            };
+            Dictionary<string, bool> emptyMap = new Dictionary<string, bool>();
+
+            SitewatchTaskConfig taskConfig = new SitewatchTaskConfig();
+            taskConfig.URL = "";
+            taskConfig.QuerySelectorAll_Query = "food";
+            taskConfig.PreprocessSteps = new List<PreprocessStep>();
+            taskConfig.ShouldWatchForAdditions = true;
+            taskConfig.ShouldWatchForDeletions = true;
+            taskConfig.ShouldWatchForNoChanges = true;
+            taskConfig.UpdateCheckIntervalSeconds = 1;
+            taskConfig.ShouldForgetDeletions = false;
+            taskConfig.name = TaskName;
+
+            //Clear any previous tests
+            await Safety.setArchivedSiteContent(taskConfig, emptyMap, emptyMap, emptyMap);
+
+            //Set initial
+            await Program.HandleUpdateLogic(taskConfig, history[0]);
+
+            //Should still contain 3
+            await Program.HandleUpdateLogic(taskConfig, history[1]);
+            count = (await Safety.getArchivedSiteContent(TaskName)).Count;
+            Assert.That(count, Is.EqualTo(3));
+
+            //Should still contain 3
+            await Program.HandleUpdateLogic(taskConfig, history[2]);
+            count = (await Safety.getArchivedSiteContent(TaskName)).Count;
+            Assert.That(count, Is.EqualTo(3));
         }
     }
 }
